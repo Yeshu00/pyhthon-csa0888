c18e2083ead91eb27ce7df10aa83a44896385f2e
Yeshu00 <113888506+Yeshu00@users.noreply.github.com>
2023-07-19 08:47:15 +0530

    ouput
    enter the age:12
    you are not eligible for voting
    you are so young you have 6 to vote

---
+++ vote eligibility.py
@@ -0,0 +1,7 @@
+age=int(input("enter the age:"))
+if (age>18):
+   print("you are eligible for voting")
+else:
+   print("you are not eligible for voting")
+wait=18-age
+print("you are so young you have",wait,"to vote")
